1ea6e7048b92620c48302ff5dc8f72d8a4142e44
Velor <velorkhalilov#gmail.com>
2020-04-19 13:25:53 +0300

    Добавил тестовый логин

--- contoller/user.php
+++ contoller/user.php
@@ -2,8 +2,14 @@
 
 class userController extends baseController {
     
-    public function index() {
+    public function index($params) {
         return $this->Json("test");
+    }
+
+    public function login($params) {
+        if (!isset($_POST["login"]) || !isset($_POST["password"])) {
+            return $this->Json("Fail");
+        }
     }
 
 }
