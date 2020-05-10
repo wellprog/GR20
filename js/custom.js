ab4995006d5f80a848b46066e2c9061a577e1f44
Velor <velorkhalilov#gmail.com>
2020-05-10 13:04:55 +0300

    Добавил слайдер

--- js/custom.js
+++ js/custom.js
@@ -162,37 +162,7 @@
     });
 	
 	
-	//Main-Slider		
-	$(window).on('load', function() {
-		$('.slider-new-2').owlCarousel({
-			items: 1,
-			navText:['<i class="fa fa-chevron-left"></i>','<i class="fa fa-chevron-right"></i>'],
-			nav:true,
-			loop: true,
-			dots: false,
-			autoplay: true,
-			autoplayTimeout: 4000,
-			animateOut: 'fadeOut',
-			responsiveRefreshRate: 0,
-			onInitialized: function(event) {
-			  centerSlideImages(event);
-			},
-			onResized: function(event) {
-			  centerSlideImages(event);
-			},
-		});
-	});
 	
-	$('.slider-new-2').on("translate.owl.carousel", function (e) {
-		$(".slider-content h2").removeClass("animated fadeInUp").css("opacity", "0");
-		$(".slider-content p").removeClass("animated zoomIn").css("opacity", "0");
-		$(".slider-content .btn-slider").removeClass("animated fadeInDown").css("opacity", "0");
-	});
-	$('.slider-new-2').on("translated.owl.carousel", function (e) {
-		$(".slider-content h2").addClass("animated fadeInUp").css("opacity", "1");
-		$(".slider-content p").addClass("animated zoomIn").css("opacity", "1");
-		$(".slider-content .btn-slider").addClass("animated fadeInDown").css("opacity", "1");
-	});
 	
 	
 	//count		
--- js/userscript.js
+++ js/userscript.js
@@ -3,13 +3,13 @@
      * 
      * @param {jQuery<any>} $ 
      */
-    function($) {
+    function ($) {
 
         let user = null;
 
         function IsLogedIn() {
             if (user == null) {
-                $.get("/user/islogin", function(responce) {
+                $.get("/user/islogin", function (responce) {
                     if (responce.isLogin == true) {
                         user = responce.user;
                     }
@@ -32,9 +32,9 @@
         }
 
 
-        $("#login_button").click(function() {
+        $("#login_button").click(function () {
             var form = $(this)[0].form;
-            $.post("/user/login", $(form).serialize(), function(data) {
+            $.post("/user/login", $(form).serialize(), function (data) {
                 if (data.error != undefined) {
                     alert(data.error);
                     return;
@@ -48,7 +48,7 @@
         });
 
 
-        $("#register_button").click(function() {
+        $("#register_button").click(function () {
             var form = $(this)[0].form;
             //Проверяем пароль
             if (form["password"].value == "") {
@@ -61,7 +61,7 @@
                 return false;
             }
 
-            $.post("/user/register", $(form).serialize(), function(data) {
+            $.post("/user/register", $(form).serialize(), function (data) {
                 if (data.error != undefined) {
                     alert(data.error);
                     return;
@@ -74,8 +74,8 @@
             return false;
         });
 
-        $("#logout_button").click(function() {
-            $.get("/user/logout", function() {
+        $("#logout_button").click(function () {
+            $.get("/user/logout", function () {
                 alert("Досвидания");
                 window.location.reload();
             })
@@ -83,10 +83,10 @@
         });
 
 
-        $("#complement_add_button").click(function() {
+        $("#complement_add_button").click(function () {
             var form = $(this)[0].form;
 
-            $.post("/complement/add", $(form).serialize(), function(data) {
+            $.post("/complement/add", $(form).serialize(), function (data) {
                 if (data.error != undefined) {
                     alert(data.error);
                     return;
@@ -96,6 +96,59 @@
                 window.location.reload();
             });
         });
+
+
+
+        $.get("/complement/all", function(data) {
+            
+            for (var i = 0; i < data.length; i++) {
+                let item = $(".template-carousel-item").clone();
+                item.removeClass("template-carousel-item");
+
+                item.find(".carousel-title").html(data[i].title);
+                item.find(".carousel-name").html(data[i].name);
+
+                $(".slider-new-2").append(item);
+            }
+
+            $('.slider-new-2').owlCarousel({
+                items: 1,
+                navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
+                nav: true,
+                loop: true,
+                dots: false,
+                autoplay: true,
+                autoplayTimeout: 4000,
+                animateOut: 'fadeOut',
+                responsiveRefreshRate: 0,
+                onInitialized: function (event) {
+                    centerSlideImages(event);
+                },
+                onResized: function (event) {
+                    centerSlideImages(event);
+                },
+            });
+            
+
+        });
+
+
+
+        // //Main-Slider		
+        // $(window).on('load', function () {
+        // });
+
+        $('.slider-new-2').on("translate.owl.carousel", function (e) {
+            $(".slider-content h2").removeClass("animated fadeInUp").css("opacity", "0");
+            $(".slider-content p").removeClass("animated zoomIn").css("opacity", "0");
+            $(".slider-content .btn-slider").removeClass("animated fadeInDown").css("opacity", "0");
+        });
+        $('.slider-new-2').on("translated.owl.carousel", function (e) {
+            $(".slider-content h2").addClass("animated fadeInUp").css("opacity", "1");
+            $(".slider-content p").addClass("animated zoomIn").css("opacity", "1");
+            $(".slider-content .btn-slider").addClass("animated fadeInDown").css("opacity", "1");
+        });
+
 
     }
 )(jQuery);
