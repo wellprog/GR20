3ab0d9a2af37a8688c96523af9b57018f7a1dcc4
Velor <velorkhalilov#gmail.com>
2020-05-10 12:45:03 +0300

    Добавил обработку добавления комплемента

--- contoller/complement.php
+++ contoller/complement.php
@@ -9,9 +9,18 @@ class complementController extends baseController {
             ]);
         }
 
+        $title = trim($_POST["title"]);
+        $name = trim($_POST["name"]);
+
+        if ($title == "" || $name == "") {
+            return $this->Json([
+                "error" => "Все поля обязательны" 
+            ]);
+        }
+
         InsertIntoDB("INSERT INTO `complement` (`title`, `name`) VALUES (:title, :name)", [
-            "title" => $_POST["title"],
-            "name" => $_POST["name"]
+            "title" => $title,
+            "name" => $name
         ]);
 
         return $this->Json(new stdClass());
--- js/userscript.js
+++ js/userscript.js
@@ -41,7 +41,7 @@
                 }
 
                 alert("Добро пожаловать!");
-                window.location.href = window.location.href;
+                window.location.reload();
             });
 
             return false;
@@ -68,7 +68,7 @@
                 }
 
                 alert("Добро пожаловать!");
-                window.location.href = window.location.href;
+                window.location.reload();
             });
 
             return false;
@@ -77,9 +77,24 @@
         $("#logout_button").click(function() {
             $.get("/user/logout", function() {
                 alert("Досвидания");
-                window.location.href = window.location.href;
+                window.location.reload();
             })
             return false;
+        });
+
+
+        $("#complement_add_button").click(function() {
+            var form = $(this)[0].form;
+
+            $.post("/complement/add", form.serialize(), function(data) {
+                if (data.error != undefined) {
+                    alert(data.error);
+                    return;
+                }
+
+                alert("Комплемент добавлен успешно");
+                window.location.reload();
+            });
         });
 
     }
