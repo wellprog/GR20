0b8ea92388e1ac5bdfa0e9444e77c505e72e6c9e
Velor <velorkhalilov#gmail.com>
2020-05-10 12:36:14 +0300

    Add php complement

---
+++ contoller/complement.php
@@ -0,0 +1,25 @@
+<?php
+
+class complementController extends baseController {
+
+    public function add($params) {
+        if ( !isset($_POST["title"]) || !isset($_POST["name"]) ) {
+            return $this->Json([
+                "error" => "Все поля обязательны" 
+            ]);
+        }
+
+        InsertIntoDB("INSERT INTO `complement` (`title`, `name`) VALUES (:title, :name)", [
+            "title" => $_POST["title"],
+            "name" => $_POST["name"]
+        ]);
+
+        return $this->Json(new stdClass());
+    }
+
+    public function all($params) {
+        $items = GetAllFromDB("SELECT * FROM `complement`");
+        return $this->Json($items);
+    }
+
+}
